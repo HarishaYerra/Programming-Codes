eb790664e2a2c31517817b59d7c7af5a3eaff7fb
YERRA HARISHA <harishayerra2001@gmail.com>
2025-10-08 22:19:31 +0530

    Initial commit - Eclipse Java project

---
+++ src/Codes/Armstrong.java
@@ -0,0 +1,7 @@
+package Codes;
+
+public class Armstrong {
+public static void main(String args[]) {
+	System.out.println("Armstrong Number");
+}
+}
---
+++ src/Codes/Printname.java
@@ -0,0 +1,7 @@
+package Codes;
+
+public class Printname {
+	public static void main(String args[]) {
+		System.out.println("Harisha");
+	}
+}
