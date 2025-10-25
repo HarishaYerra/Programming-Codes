cf0373e1d645fb2fbab05ff28b0f94b4ce3e9e00
YERRA HARISHA <harishayerra2001@gmail.com>
2025-10-25 15:22:04 +0530

    Initial Commit Program16

--- src/Codes/Problem16.java
+++ src/Codes/Problem16.java
@@ -1,28 +1,27 @@
-/*Program-2
-Create a class 'RowSum' containing a method that calculating row-wise sum of array of size 3 X 3
-Method Name :getRowSum() 
+/*Program-3
+Create a class that contains a method which takes two-dimensional array of integers(array has equal number of rows and columns) and calculate the sum of diagonal element starting from 1st column
+
+Method Name :getDiagonalDataSum() 
 Argument	: int[][]
-Return type: int[]
+Return type: int
 For Example
 Input:
-1 2 3
-1 2 3
-1 2 3 
+1 2 
+3 4
 Output:
-6 6 6
+5
 */
-
 
 package Codes;
 
 import java.util.*;
 
-public class Problem15 {
+public class Problem16 {
     public static void main(String args[]) {
 
         Scanner sc = new Scanner(System.in);
         int arr1[][] = new int[3][3];
-        int rowsum[] = new int[3]; // to store row sums
+        int diagonalsum =0; // to store row sums
 
         System.out.println("Enter 9 numbers for 3x3 matrix:");
 
@@ -35,18 +34,14 @@ public class Problem15 {
 
         // calculating row-wise sum
         for(int i=0; i<3; i++){
-            int sum = 0;
             for(int j=0; j<3; j++){
-                sum += arr1[i][j];
+            	if(i==j)
+            		diagonalsum += arr1[i][j];
             }
-            rowsum[i] = sum;
         }
 
-        // printing output
-        System.out.println("Row-wise Sum:");
-        for(int i=0; i<3; i++){
-            System.out.print(rowsum[i] + " ");
-        }
+        System.out.println(diagonalsum);
     }
 }
+
 
