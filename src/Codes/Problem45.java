eada858b3fc61f4e0b0bc4e57cb3b5568dafd579
YERRA HARISHA <harishayerra2001@gmail.com>
2025-12-06 21:20:45 +0530

    Initial Commit Problem45

--- src/Codes/Problem45.java
+++ src/Codes/Problem45.java
@@ -1,4 +1,15 @@
+
+/*1) Prefix Sum / Kadane / Subarrays
+Maximum Subarray (Kadane’s Algorithm)
+
+Description: Given an integer array, find the contiguous subarray (containing at least one number) 
+which has the largest sum and return its sum.
+Typical constraints: O(n) time, O(1) extra space.
+Example: Input: [-2,1,-3,4,-1,2,1,-5,4] → Output: 6 (subarray [4,-1,2,1])
+
+*/
 package Codes;
+
 
 public class Problem45 {
 
