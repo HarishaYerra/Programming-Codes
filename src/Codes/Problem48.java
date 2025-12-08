8ca3471fb2b014c0d9f78e362dd1b17aadb3fcf9
YERRA HARISHA <harishayerra2001@gmail.com>
2025-12-08 13:14:40 +0530

    Initial Commit Problem48

---
+++ src/Codes/Problem48.java
@@ -0,0 +1,30 @@
+
+/*Longest Subarray with Sum K
+
+Description: Given an array of integers (can be positive, negative, zero)
+ and an integer K, find the length of the longest contiguous subarray whose sum equals K.
+Typical approach: Prefix-sum + HashMap storing earliest index for each prefix sum.
+Example: Input: nums = [1,-1,5,-2,3], K = 3 → Output: 4 (subarray [1,-1,5,-2])*/
+
+package Codes;
+
+import java.util.*;
+
+public class Problem48 {
+	public static void main(String args[]) {
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter array Size:");
+		int n=sc.nextInt();
+		int arr[]=new int[n];
+		int arr1[]=new int[n];
+		System.out.println("Enter Array elements:");
+		for(int i=0;i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		
+		for(int i=0;i<arr.length;i++) {
+			for(int j=)
+		}
+		
+	}
+}
