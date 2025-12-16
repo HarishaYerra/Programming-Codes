8ea64a50048c32777996cf07d5a6e4adf0760469
YERRA HARISHA <harishayerra2001@gmail.com>
2025-12-16 13:03:26 +0530

    Modified Problem52

--- src/Codes/Problem52.java
+++ src/Codes/Problem52.java
@@ -8,6 +8,44 @@ Example: Input: [1,8,6,2,5,4,8,3,7] → Output: 49*/
 
 package Codes;
 
-public class Problem52 {
+import java.util.*;
 
+public class Problem52 {
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+
+	        System.out.println("Enter number of heights:");
+	        int n = sc.nextInt();
+
+	        int[] height = new int[n];
+	        System.out.println("Enter heights:");
+	        for (int i = 0; i < n; i++) {
+	            height[i] = sc.nextInt();
+	        }
+
+	        int maxArea = maxArea(height);
+	        System.out.println("Maximum area: " + maxArea);
+	    }
+	 public static int maxArea(int[] height) {
+	        int left = 0;
+	        int right = height.length - 1;
+	        int max = 0;
+
+	        while (left < right) {
+	            int width = right - left;
+	            int h = Math.min(height[left], height[right]);
+	            int area = width * h;
+
+	            max = Math.max(max, area);
+
+	            // Move the smaller height inward
+	            if (height[left] < height[right]) {
+	                left++;
+	            } else {
+	                right--;
+	            }
+	        }
+
+	        return max;
+	 }
 }
